f46e45a5f66974b811af5a24e1ab6f174aa96ec4
Fetra01 <fetra.andria@outlook.fr>
2020-11-05 15:28:55 +0100

    Initialisation du projet react-crud

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAttuRAMI0xGFw_YnmLgmX0mvj0gjRKtF0",
+    authDomain: "react-auth-de663.firebaseapp.com",
+    
+    //databaseURL: "https://react-auth-de663.firebaseio.com",
+    //projectId: "react-auth-de663",
+    //storageBucket: "react-auth-de663.appspot.com",
+    //messagingSenderId: "258489500067",
+    //appId: "1:258489500067:web:56f681c309109fd7411a16"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
